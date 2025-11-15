fb543860779717c52add86a83d73d84ab219abac
Swanchick <61909965+Swanchick@users.noreply.github.com>
2025-11-15 21:44:57 +0200

    update

--- lua/ulx/modules/sh/discord.lua
+++
@@ -1,8 +0,0 @@
--- відправляє клієнту код для висирання повідомлення в чат.
-function ulx.discord(calling_ply)
-	calling_ply:SendLua([[chat.AddText(Color(135,221,255), "Наш діскорд: ", Color(221,180,255), "https://discord.gg/stV4JswQ9Q")]])
-end
-
-local discord = ulx.command("Prostir", "ulx discord", ulx.discord, "!discord")
-discord:defaultAccess(ULib.ACCESS_ALL)
-discord:help("Дає посилання на діскорд сервер Простору.")
